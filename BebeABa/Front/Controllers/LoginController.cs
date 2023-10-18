79bb986a75c35f4be06991f17046206604ea76dc
Ecxpectro <henriquescunha12345@gmail.com>
2023-10-18 16:50:45 -0300

    First Configurations

---
+++ BebeABa/Front/Controllers/LoginController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Front.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public LoginController(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ BebeABa/Shared/ApiUtilities/HostService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpareParts.Shared.ApiUtilities
+{
+    public class HostService
+    {
+        public string Host { get; set; }
+    }
+}
