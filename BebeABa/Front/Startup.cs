51e348ee49094edbdb93015f7069d761b4dff23c
henrique.schraiber <henrique.schraiber@autolab.live>
2023-11-26 10:08:14 -0300

    test

--- BebeABa/Front/Startup.cs
+++ BebeABa/Front/Startup.cs
@@ -21,7 +21,7 @@ namespace Front
         }
 
         public IConfiguration Configuration { get; }
-
+        //test
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
